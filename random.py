4f548519b4b06488eacfe052f858784d6b52aeb6
spring <s2018w26@e-mirim.hs.kr>
2019-09-03 11:08:30 +0900

    random

---
+++ random.py
@@ -0,0 +1,27 @@
+import random
+
+print('0 이상 1 미만 실수 값')
+print('random.random() : ', random.random())
+print()
+print('시작값 2.5 이상 끝값 10.0 미만 실수 값')
+print('0 이상 끝값 10 미만 정수 값')
+print('random.randramge(10) : ', random.randramge(10))
+print()
+print('random.randramge(1,7,2) : ', random.randrange(1,7,2))
+print()
+print('리스트에서 1개 값 꺼내오기')
+season = ['봄', '여름', '가을', '겨울']
+print('season = ', season)
+print('random.choice(season) : ', random.choice(season))
+print()
+l = ['가', '나', '다', '라', '마']
+print('리스트 순서를 섞기')
+print('섞기 전 l = ', l)
+random.shuffle(l)
+print('random.shuffle(l)')
+print('섞은 후 ㅣ = ', l)
+print()
+print('리스트에서 몇 개의 값을 중복하지 않고 3개 뽑기')
+sample = ['1번', '2번', '3번', '4번', '5번', '6번', '7번', '8번', '9번']
+print('샘플 대상 = ', sample)
+print('random.sample(sample,3) : ', random.sample(sample,3))
